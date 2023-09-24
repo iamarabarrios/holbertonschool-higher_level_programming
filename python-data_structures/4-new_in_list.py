aff4027027b4a5ab2beb4380d666189796b17f8d
iamara <iamarabarrios457@gmail.com>
2023-09-24 13:00:13 -0700

    4-new_in_list.py

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    other_list = my_list.copy()
+
+    if idx < 0 or idx >= len(my_list):
+        return other_list
+    other_list[idx] = element
+    return other_list
