059c66bcc2e1df6402fa585f91e9975264b4c9de
iamara <iamarabarrios457@gmail.com>
2023-09-24 12:54:50 -0700

    3-print_reversed_list_integer.py

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+
+    if my_list:
+        other_list = my_list.copy()
+        other_list.reverse()
+
+        for i in other_list:
+            print("{}".format(i))
