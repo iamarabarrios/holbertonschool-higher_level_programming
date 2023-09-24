f468cd56b11a7b36c310fb5bc50706aa6b86d95a
iamara <iamarabarrios457@gmail.com>
2023-09-24 13:04:38 -0700

    none

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,8 +2,6 @@
 
 
 def element_at(my_list, idx):
-    if idx <= 0:
-        return
-    if idx >= len(my_list):
-        return
+    if idx < 0 >= len(my_list):
+        return none
     return my_list[idx]
