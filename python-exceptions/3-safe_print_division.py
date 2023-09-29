2fd66af9e22b502027afae429a71dab523f4a40c
iamara <iamarabarrios457@gmail.com>
2023-09-29 07:22:02 -0700

    3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         division = None
     finally:
-        print("Inside Result: {}".format(division))
-        return division
+        print("Inside result: {}".format(division))
+    return division
