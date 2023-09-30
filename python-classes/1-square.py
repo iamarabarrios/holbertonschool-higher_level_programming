318b6af1fb0ba96340bbf6f47ef8eb97a83c486d
iamara <iamarabarrios457@gmail.com>
2023-09-30 15:52:54 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    def __init__(self, size):
+        """Initialize new Square"""
+        self.__size = size
