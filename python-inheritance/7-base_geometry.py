d051e18b5ccef9de7bd2e9f8e66993f654bfb637
iamara <iamarabarrios457@gmail.com>
2023-10-06 07:51:20 -0700

    7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            ValueError("{} must be greater than 0".format(name)")
