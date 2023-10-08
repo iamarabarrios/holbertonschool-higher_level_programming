e9f121c77c0eaaf0e2f772620a01344ec7420395
iamara <iamarabarrios457@gmail.com>
2023-10-08 15:30:24 -0700

    8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,9 +2,8 @@
 """Defines a JSON file-reading function."""
 
 
-import json
-
-
 def class_to_json(obj):
-    """Convert an instance of MyClass to a JSON serializable dictionary"""
-    return obj.__dict__
+    """
+    Returns the dictionary description."""
+    return {key: value for key, value in obj.__dict__.items() if
+            isinstance(value, (list, dict, str, int, bool))}
