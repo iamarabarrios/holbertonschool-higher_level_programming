14701ebc86504b2d720037d0aec3c459d60a0972
iamara <iamarabarrios457@gmail.com>
2023-10-08 16:35:41 -0700

    7-add_item.py changes

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,9 +3,18 @@
 
 
 import json
+import sys
 
 
 def save_to_json_file(my_obj, filename):
     """Return the JSON representation of string object."""
     with open(filename, mode="w") as file:
         json.dump(my_obj, file)
+
+if __name__ == "__main__":
+    my_list = []
+
+    for arg in sys.argv[1:]:
+        my_list.append(arg)
+
+    save_to_json_file(my_list, "add_item.json")
