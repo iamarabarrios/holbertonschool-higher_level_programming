1ab2fe1f28aabd99b51478a2db83ca44bab69b33
iamara <iamarabarrios457@gmail.com>
2023-10-08 16:58:33 -0700

    changes syntax

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -35,10 +35,10 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
-        return ((self.__width * 2) +(self.__height * 2))
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
@@ -50,4 +50,4 @@ class Rectangle:
             if i != self.__height - 1:
                 rectangle.append("\n")
 
-        return  ("".join(rectangle))
+        return ("".join(rectangle))
