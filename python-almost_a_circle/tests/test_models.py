a2e216ea868e6cce4ab7395a348255148cabbddb
iamarabarrios <iamarabarrios457@gmail.com>
2023-10-09 17:59:23 -0700

    test_models.py

--- python-almost_a_circle/tests/test_models.py
+++ python-almost_a_circle/tests/test_models.py
@@ -13,6 +13,7 @@ class TestBase(unittest.TestCase):
         base_instance = Base(5)
         self.assertEqual(base_instance.id, 5)
 
+
 class TestRectangle(unittest.TestCase):
     """Test rectangle."""
     def test_rectangle_constructor(self):
@@ -20,3 +21,18 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rectangle_instance.width, 10)
         self.assertEqual(rectangle_instance.height, 20)
 
+    def test_invalid_rectangle_width_type(self):
+        with self.assertRaises(TypeError):
+            rectangle_instance = Rectangle("invalid", 20)
+
+    def test_invalid_rectangle_width_value(self):
+        with self.assertRaises(ValueError):
+            rectangle_instance = Rectangle(-5, 20)
+
+    def test_invalid_rectangle_x_type(self):
+        with self.assertRaises(TypeError):
+            rectangle_instance = Rectangle(10, 20, "invalid_x")
+
+    def test_invalid_rectangle_x_value(self):
+        with self.assertRaises(ValueError):
+            rectangle_instance = Rectangle(10, 20, -5)
