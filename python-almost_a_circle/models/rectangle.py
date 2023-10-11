bce5fb132be9e503c08a99edd5fcb9913dc1e42b
iamarabarrios <iamarabarrios457@gmail.com>
2023-10-10 18:33:12 -0700

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -100,3 +100,8 @@ class Rectangle(Base):
             for s in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """Actualize the class rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
