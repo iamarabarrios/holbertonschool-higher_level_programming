5e7e744d70dc8adccdec162f1fe3e58b8dacabe2
iamarabarrios <iamarabarrios457@gmail.com>
2023-10-11 07:25:11 -0700

    square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -39,3 +39,13 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 if key in attributes:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return the representation of dictionary."""
+        return {
+                "id": self.id,
+                "width": self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
+        }
