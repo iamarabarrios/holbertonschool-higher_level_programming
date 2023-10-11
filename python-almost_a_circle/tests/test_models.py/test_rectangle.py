2450e00b048ff33ef6b5809bca0366e11d489f48
iamarabarrios <iamarabarrios457@gmail.com>
2023-10-10 17:10:09 -0700

    test_models.py

--- python-almost_a_circle/tests/test_models.py/test_rectangle.py
+++ python-almost_a_circle/tests/test_models.py/test_rectangle.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-"""Tests."""
+"""Tests for the Rectangle class."""
 
-
-from models.base import Base
 from models.rectangle import Rectangle
 import unittest
 
-
-class TestBase(unittest.TestCase):
-    """Tests base."""
-    def test_base_constructor_with_id(self):
-        base_instance = Base(5)
-        self.assertEqual(base_instance.id, 5)
-
-
 class TestRectangle(unittest.TestCase):
-    """Test rectangle."""
+    """Tests for the Rectangle class."""
+    
     def test_rectangle_constructor(self):
         rectangle_instance = Rectangle(10, 20)
         self.assertEqual(rectangle_instance.width, 10)
@@ -40,3 +31,6 @@ class TestRectangle(unittest.TestCase):
     def test_area(self):
         rectangle_instance = Rectangle(10, 20)
         self.assertEqual(rectangle_instance.area(), 200)
+
+if __name__ == "__main__":
+    unittest.main()
