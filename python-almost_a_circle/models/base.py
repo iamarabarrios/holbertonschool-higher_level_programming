c66b78bd16e573647dbc78bf066760578dabb292
iamarabarrios <iamarabarrios457@gmail.com>
2023-10-11 07:32:25 -0700

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """Write the first class Base."""
 
 
+import jason
+
+
 class Base:
     """This is the base class."""
     __nb_objects = 0
@@ -13,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the json string representation of diciotnary list."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
