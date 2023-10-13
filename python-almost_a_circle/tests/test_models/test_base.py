4536f041458da50754565404e27554a1c6fe7183
iamarabarrios <iamarabarrios457@gmail.com>
2023-10-13 15:00:51 -0700

    test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """Tests for the Base class."""
 
-from models.base import Base
+
 import unittest
+from models.base import Base
+
 
 class TestBase(unittest.TestCase):
     """Tests for the Base class."""
-    
-    def test_base_constructor_with_id(self):
+
+    def testBaseConstructorWithId(self):
         base_instance = Base(5)
         self.assertEqual(base_instance.id, 5)
+
 
 if __name__ == "__main__":
     unittest.main()
