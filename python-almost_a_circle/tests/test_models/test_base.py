c5fe0d8480c426bfc390e70d42ff7b1aa2b189cf
iamarabarrios <iamarabarrios457@gmail.com>
2023-10-13 15:43:16 -0700

    test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -10,6 +10,7 @@ class TestBase(unittest.TestCase):
     """Tests for the Base class."""
 
     def testBaseConstructorWithId(self):
+        """Test constructor."""
         base_instance = Base(5)
         self.assertEqual(base_instance.id, 5)
 
@@ -18,6 +19,7 @@ if __name__ == "__main__":
     unittest.main()
 
     def testToJSONString(self):
+        """test to json string."""
         data = [{"id": 1, "name": "Object1"}, {"id": 2, "name": "Object2"}]
         json_str = Base.to_json_string(data)
         expected_json = (
