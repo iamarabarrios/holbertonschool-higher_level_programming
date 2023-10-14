20e268dd96aacb04d0632149ed458f0594a00a0d
iamarabarrios <iamarabarrios457@gmail.com>
2023-10-13 17:43:22 -0700

    test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -26,3 +26,22 @@ if __name__ == "__main__":
             '[{"id": 1, "name": "Object1"}, {"id": 2, "name": "Object2"}]'
         )
         self.assertEqual(json_str, expected_json)
+
+    def testToJSONStringWithEmptyList(self):
+        """Prueba de to_json_string con lista vacía."""
+        data = []
+        json_str = Base.to_json_string(data)
+        expected_json = "[]"
+        self.assertEqual(json_str, expected_json)
+
+    def testCreateMethod(self):
+        """Prueba del método create."""
+        attributes = {"id": 3, "name": "Object3"}
+        instance = Base.create(**attributes)
+        self.assertEqual(instance.id, 3)
+        self.assertEqual(instance.name, "Object3")
+
+    def testSaveToFile(self):
+        """Prueba de save_to_file."""
+        data = [{"id": 1, "name": "Object1"}, {"id": 2, "name": "Object2"}]
+        Base.save_to_file(data)
