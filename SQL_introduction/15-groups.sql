b026575c8ca8be6743d6601496e36a34daf9db45
iamarabarrios <iamarabarrios457@gmail.com>
2023-11-10 20:14:23 -0300

    exercise 15 alias, exercise 16 is not null

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Lists the number of records with the same score in the table second_table.
-SELECT score, COUNT(*) as number
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER BY score Desc;
