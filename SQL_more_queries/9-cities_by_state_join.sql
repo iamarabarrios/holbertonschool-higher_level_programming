eddd5ede52816b70e2da989df58f08b897e43ee1
iamarabarrios <iamarabarrios457@gmail.com>
2023-11-12 16:20:37 -0300

    delete use

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- Lists all cities in the hbtn_0d_usa database.
-USE hbtn_0d_usa;
-SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT c.id, c.name, s.name
+FROM cities AS c
+JOIN states AS s ON c.state_id = s.id
+ORDER BY c.id ASC;
