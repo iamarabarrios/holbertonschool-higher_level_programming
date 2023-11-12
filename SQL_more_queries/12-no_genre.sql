05d77f4072411ff2dda322255cec5e9863b02c21
iamarabarrios <iamarabarrios457@gmail.com>
2023-11-12 16:38:13 -0300

    import database

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- Import the database dump from hbtn_0d_tvshows.
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tsg ON ts.id = tsg.show_id
+ORDER BY ts.title, tsg.genre_id ASC;
